f5e29a816d846db51864f09479b6067fd51851e9
amanyadav-work <amansyadavwork@gmail.com>
2025-04-27 21:56:28 +0530

    db-Uri

--- src/utils/dbConnect.js
+++ src/utils/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/rpg';
+const MONGO_URI = process.env.MONGODB_ATLAS_URI || 'mongodb://127.0.0.1:27017/rpg';
 
 let isConnected = false;
 
